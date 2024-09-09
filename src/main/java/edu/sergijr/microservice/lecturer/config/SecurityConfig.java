b088c57c8fd877b8ae0907432129070dfd2787c4
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-09-09 13:59:36 +0400

    temporary disabled csrf

--- src/main/java/edu/sergijr/microservice/lecturer/config/SecurityConfig.java
+++ src/main/java/edu/sergijr/microservice/lecturer/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +13,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain authorizeRequestsFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                 .anyRequest().permitAll());
         return http.build();
     }
