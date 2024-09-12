7c9756f6737becfe75ab6663d5bf44790905a29a
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-09-12 14:37:32 +0400

    configurations have been moved to the configuration server. Added dependencies for the configuration server

--- src/main/java/edu/sergijr/microservice/lecturer/MicroserviceLecturerApplication.java
+++ src/main/java/edu/sergijr/microservice/lecturer/MicroserviceLecturerApplication.java
@@ -2,10 +2,12 @@ package edu.sergijr.microservice.lecturer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@RefreshScope
 public class MicroserviceLecturerApplication {
 
     public static void main(String[] args) {
